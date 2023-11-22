cfb80da8abfaf066d352af566c74217bd9611a3f
nilufer.polat <nilufer.polat@pinsoft.ist>
2023-11-22 10:32:51 +0300

    component scan eklendi

--- src/main/java/com/budgettracker/App/AppApplication.java
+++ src/main/java/com/budgettracker/App/AppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.budgettracker.App.Controller")
 public class AppApplication {
 
 	public static void main(String[] args) {
